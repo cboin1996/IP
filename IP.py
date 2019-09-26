bc52e5031845a80d07a9883252d2aaba2b58e1ab
cboin1996 <christianboin@hotmail.com>
2019-09-25 22:33:58 -0400

    wrote IP listener

--- IP.py
+++ IP.py
@@ -3,21 +3,39 @@ import os
 import json
 import time
 import Message
-    
+import sys
+import logging
+#setup loggin for server script
+pathToFolder = os.path.dirname(os.path.realpath(__file__))
+logger = logging.getLogger('IP.py')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(fmt='%(asctime)s %(name)s  %(levelname)-8s %(message)s',datefmt="%Y-%m-%d - %H:%M:%S")
+fh = logging.FileHandler(os.path.join(pathToFolder, 'IPHistory.log'))
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 def getIp():
     try:
-        return requests.get('https://api.ipify.org').text
+        req = requests.get('https://api.ipify.org').text
+        return req
     except:
-        print("Could not connect to website")
+        logger.info("Request: %s, Could not connect to website" % (req))
 
 
-def main():
+def main(arguments):
     messager = Message.Messager()
+    delay = float(sys.argv[1])
+    while True:
+        lastIp = getIp()
+        time.sleep(delay*3600)
+        currentIp = getIp()
+        if lastIp != currentIp:
+            logger.info("IP change detected (%s --> %s).  Sending update text." % (lastIp, currentIp))
+            messager.createSMS(currentIp)
+        else:
+            logger.info("No change in IP.. Last ext. Ip was: %s, current is: %s." % (lastIp, currentIp))
 
-    #while True:
-    ip = getIp()
-    messager.createSMS(ip)
-
+        
 if __name__=="__main__":
     
-    main()
+    main(sys.argv)
