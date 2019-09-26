0595afe7eba6605f6de32b30bbd1452be1ddc8f3
cboin1996 <christianboin@hotmail.com>
2019-09-25 22:11:13 -0400

    test'

--- IP.py
+++ IP.py
@@ -1,7 +1,23 @@
 import requests
-
+import os
+import json
+import time
+import Message
+    
 def getIp():
-    print("Hello")
+    try:
+        return requests.get('https://api.ipify.org').text
+    except:
+        print("Could not connect to website")
+
+
+def main():
+    messager = Message.Messager()
+
+    #while True:
+    ip = getIp()
+    messager.createSMS(ip)
 
 if __name__=="__main__":
-    getIp()
+    
+    main()
